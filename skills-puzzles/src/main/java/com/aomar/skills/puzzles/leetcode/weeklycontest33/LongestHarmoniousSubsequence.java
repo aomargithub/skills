437ccedd34c20a26b91beb42b7776a08c822b89f
aomargithub <aomar.github@gmail.com>
2017-05-28 10:59:55 +0300

    init

---
+++ skills-puzzles/src/main/java/com/aomar/skills/puzzles/leetcode/weeklycontest33/LongestHarmoniousSubsequence.java
@@ -0,0 +1,24 @@
+package com.aomar.skills.puzzles.leetcode.weeklycontest33;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestHarmoniousSubsequence {
+	
+	public int findLHS(int[] nums) {
+		
+		int lhs = 0;
+		
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for(int num : nums){
+			map.put(num, map.getOrDefault(num, 0) + 1);
+		}
+		
+		for(int num : map.keySet()){
+			if(map.containsKey(num + 1)){
+				lhs = Math.max(lhs, map.get(num + 1) + map.get(num));
+			}
+		}
+		return lhs;		
+	}
+}
